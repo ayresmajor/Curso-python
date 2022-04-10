006229cec38ea365bf43b19e3ce93fbd32e1dca6
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-04-10 16:18:26 +0100

    update do programa

--- Scrip Python/Sequência sem repetição de randint.py
+++ Scrip Python/Sequência sem repetição de randint.py
@@ -1,34 +1,25 @@
 from random import randint
 
-block = []
-block2 = []
+lista = []
+lista2 = []
 num = []
 
 
 for c in range(6):
-    pos1 = c - 2
-    pos2 = c - 1
-    print('')
-    print("Print do c :", c)
-    print("Print do pos1 e 2: ", pos1, pos2)
     n = randint(1, 5)
-    print("Primeiro n: ", n )
+    print ('===================')
+    print("n: ", n )
     if c > 0:
-      block.append(n)
-      if c > 1:
-          while block[pos1] == block[pos2]:
-              n = randint(1, 5)
-              block[pos2] = n
-      print("Primeiro Block: ", block)
-    if len(block) > 2:
-        block2 = block[0: c -1]
-        print("Block2: ", block2)
-        while  n in block2:
-            d = block.index(n)
+      lista.append(n)
+      print("lista: ", lista)
+    if len(lista) >= 2:
+        lista2 = lista[0: c -1]
+        print("lista2: ", lista2)
+        while  n in lista2:
+            d = lista.index(n)
             n = randint(1, 5)
-            block[d] = n
-            print("n modificado: ", n)
-            print("block modificado: ", block)
+            print('n modificado2: ', n)
+            lista[d] = n
 print('Valor final abaixo:')
-block.sort()
-print(block)
+lista.sort()
+print(lista)
